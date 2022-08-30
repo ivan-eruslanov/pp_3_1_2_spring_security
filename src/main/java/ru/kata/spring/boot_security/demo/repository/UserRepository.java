0ad6aa9f81f46b86cbb77b2a7c78e5683a1d5eae
iv.eruslanov <iv.eruslanov@yandex.ru>
2022-08-30 22:39:17 +0500

    made changes RoleRepo UserRepo

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package ru.kata.spring.boot_security.demo.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.kata.spring.boot_security.demo.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT c FROM User c WHERE c.username = ?1")
-    User getUserByUsername(String username);
+    @EntityGraph(attributePaths = {"roles"})
+    User getByUsername(String username);
 }
